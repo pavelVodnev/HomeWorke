3b414248cb7a12a6a2dcd75b790e0c785aa3bf5a
pavelV <psvodnev@gmail.com>
2022-08-30 16:50:55 +0300

    Task 50,принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.

---
+++ Task_50/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 50. 
+// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же 
+// указание, что такого элемента нет.
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// 1, 7 -> такого элемента в массиве нет
+
+
+Console.WriteLine("Введите целое число");
+int x = Convert.ToInt32(Console.ReadLine()) - 1;
+int y = Convert.ToInt32(Console.ReadLine()) - 1;
+
+
+int[,] CreateArrayRnd(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(min, max);
+        }
+
+    }
+    return matrix;
+}
+
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],2}, ");
+            else Console.Write($"{matrix[i, j],2}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void FindElement(int[,] matrix)
+{
+
+    if (x <= matrix.GetLength(0) && y <= matrix.GetLength(1)) Console.WriteLine(matrix[x, y]);
+    else Console.WriteLine("Такого элемента нет");
+    return;
+}
+
+
+int[,] arr = CreateArrayRnd(3, 4, -9, 9);
+PrintArray(arr);
+FindElement(arr);
